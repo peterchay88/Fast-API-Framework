43ac4fd3f62b09aace7bee2457301a5b54e45662
Peter Chay <peterchay88@gmail.com>
2024-02-06 23:11:17 -0700

    Changes to request_utility.py

--- rest_api_test_framework/src/utilities/request_utility.py
+++ rest_api_test_framework/src/utilities/request_utility.py
@@ -1,4 +1,5 @@
 import requests
+import logging as logger
 
 
 class RequestsApiCall:
@@ -6,6 +7,25 @@ class RequestsApiCall:
     def __init__(self):
         self.url = "http://localhost:8080/docs#/"
 
+    def expected_status_code(self, status_code, expected_status_code=200):
+        """
+        This method is used to assert if the expected status code was returned from the API response call
+        :param status_code:
+        :param expected_status_code:
+        :return:
+        """
+        logger.debug(f"Checking to see if expected status code {expected_status_code} satisfied")
+        assert status_code == expected_status_code, \
+            f"Unexpected status code. Expected {expected_status_code}. Actual {status_code}."
+
     def get(self, endpoint):
+        """
+        API wrapper for the GET response call
+        :param endpoint:
+        :return:
+        """
+        logger.debug("huh?")
         response = requests.get(url=f"{self.url}{endpoint}")
+        self.expected_status_code(status_code=response.status_code)
+        logger.info("WHAT")
         return response
