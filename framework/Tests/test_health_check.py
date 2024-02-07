2c9e4c86cb4dfa9cdeaafc5e49abd42ca1648326
Peter Chay <peterchay88@gmail.com>
2024-02-06 19:14:56 -0700

    Trying stuff to resolve the no module found error. Still not able to figure it out.

---
+++ framework/Tests/test_health_check.py
@@ -0,0 +1,15 @@
+import pytest
+from framework.src.utilities.request_utility import RequestsApiCall
+
+pytestmark = [pytest.mark.health]
+
+
+class TestHealthEndpoint:
+
+    def __init__(self):
+        self.api_call = RequestsApiCall()
+        self.health_endpoint = "health"
+
+    @pytest.mark.tcid01
+    def test_get_health_endpoint(self):
+        response = self.api_call.get(endpoint=self.health_endpoint)
