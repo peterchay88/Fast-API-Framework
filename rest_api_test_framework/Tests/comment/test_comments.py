ca2f8bc259fea3b37e07788cfaccb1dc01209030
Peter Chay <peterchay88@gmail.com>
2024-03-18 16:30:46 -0600

    Updated the logic to get an auth token to be a fixture

--- rest_api_test_framework/Tests/comment/test_comments.py
+++ rest_api_test_framework/Tests/comment/test_comments.py
@@ -1,25 +1,20 @@
 import pytest
 import logging as logger
 from rest_api_test_framework.src.utilities.request_utility import RequestsApiCall
-from rest_api_test_framework.src.helpers.auth_helper import GetAuthToken
 
 pytestmark = [pytest.mark.comments]
 api_request_call = RequestsApiCall()
-auth_token = GetAuthToken()
+# auth_token = GetAuthToken()
 
 
 class TestCommentsEndpoint:
 
     @pytest.mark.tcid02
-    def test_get_comments(self):
+    def test_get_comments(self, auth_token):
         """
         This test checks to see if we can successfully run a get call on the comments endpoint
         :return:
         """
-        # Authenticate
-        token = auth_token.get_auth_token()
-
-        # Get comments
-        token_header = {"Authorization": f"Bearer {token}"}
+        token_header = {"Authorization": f"Bearer {auth_token}"}
         api_response = api_request_call.get(endpoint="comments/", api_headers=token_header)
         api_request_call.expected_status_code(status_code=api_response.status_code)
--- rest_api_test_framework/Tests/conftest.py
+++ rest_api_test_framework/Tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from rest_api_test_framework.src.utilities.request_utility import RequestsApiCall
 import logging as logger
 
@@ -9,9 +10,15 @@ class GetAuthToken:
         self.auth_endpoint = "auth/login"
         self.api_payload = {"username": "admin", "password": "admin"}
 
+    @pytest.fixture(scope="session")
     def get_auth_token(self):
         logger.debug("Fetching API token")
-        api_token = self.api_request.post(endpoint=self.auth_endpoint, api_data=self.api_payload)
+        api_token = self.api_request.post(endpoint="auth/login", api_data=self.api_payload)
         self.api_request.expected_status_code(status_code=api_token.status_code)
         logger.debug(f"API token fetched: {api_token.json()['access_token']}")
-        return api_token.json()['access_token']
+        yield api_token.json()['access_token']
+
+
+@pytest.fixture(scope="class")
+def auth_token():
+    return GetAuthToken
