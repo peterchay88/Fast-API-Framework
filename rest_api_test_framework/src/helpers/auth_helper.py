11fbbe05aae7c1d8df2911a1faa640c2e4fd1032
Peter Chay <peterchay88@gmail.com>
2024-03-18 15:08:08 -0600

    Simplified auth_helper.py

--- rest_api_test_framework/src/helpers/auth_helper.py
+++ rest_api_test_framework/src/helpers/auth_helper.py
@@ -7,15 +7,10 @@ class GetAuthToken:
     def __init__(self):
         self.api_request = RequestsApiCall()
         self.auth_endpoint = "auth/login"
-        self.api_data = "'grant_type=&username=admin&password=admin&scope=&client_id=&client_secret='"
-        self.headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        self.api_payload = {"username": "admin", "password": "admin"}
 
     def get_auth_token(self):
         logger.debug("Fetching API token")
-        api_token = self.api_request.post(endpoint=self.auth_endpoint, api_data=self.api_data,
-                                          api_headers=self.headers)
+        api_token = self.api_request.post(endpoint=self.auth_endpoint, api_data=self.api_payload)
         logger.debug(f"API token fetched: {api_token.json()['access_token']}")
         return api_token.json()['access_token']
