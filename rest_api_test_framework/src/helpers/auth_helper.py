f527308c327f4ab04c76c08fdb7abe51f6ba52a4
Peter Chay <peterchay88@gmail.com>
2024-03-18 15:10:28 -0600

    added assert

--- rest_api_test_framework/src/helpers/auth_helper.py
+++ rest_api_test_framework/src/helpers/auth_helper.py
@@ -12,5 +12,6 @@ class GetAuthToken:
     def get_auth_token(self):
         logger.debug("Fetching API token")
         api_token = self.api_request.post(endpoint=self.auth_endpoint, api_data=self.api_payload)
+        self.api_request.expected_status_code(status_code=api_token.status_code)
         logger.debug(f"API token fetched: {api_token.json()['access_token']}")
         return api_token.json()['access_token']
