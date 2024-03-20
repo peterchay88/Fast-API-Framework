a9fb19ca62f35dbda2a4150799a961fb67da05a0
Peter Chay <peterchay88@gmail.com>
2024-03-19 22:03:33 -0600

    updated line 10 to use absolute path

--- rest_api_test_framework/Tests/conftest.py
+++ rest_api_test_framework/Tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-
 import pytest
 from rest_api_test_framework.src.utilities.request_utility import RequestsApiCall
 import logging as logger
@@ -7,7 +6,8 @@ from dotenv import load_dotenv
 
 api_request = RequestsApiCall()
 
-load_dotenv("../../secrets.env")
+# Must use absolute path for env variables to work when calling from fixture
+load_dotenv("/Users/peter/Desktop/python/udemy/Rest_API_Project/secrets.env")
 admin_username = os.getenv("ADMIN_USERNAME")
 admin_password = os.getenv("ADMIN_PASSWORD")
 
@@ -15,7 +15,7 @@ admin_password = os.getenv("ADMIN_PASSWORD")
 @pytest.fixture(scope="session")
 def get_auth_token():
     logger.debug("Fetching API token")
-    payload = {"username": {admin_username}, "password": {admin_password}}
+    payload = {"username": admin_username, "password": admin_password}
     logger.debug(f"sending following payload: {payload}")
     api_token = api_request.post(endpoint="auth/login", api_data=payload)
     api_request.expected_status_code(status_code=api_token.status_code)
