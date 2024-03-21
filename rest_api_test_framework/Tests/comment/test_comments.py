a6b9653b741a34198571b7281a0e43b41fceba38
Peter Chay <peterchay88@gmail.com>
2024-03-20 20:48:14 -0600

    Finished first part of test case 3

--- rest_api_test_framework/Tests/comment/test_comments.py
+++ rest_api_test_framework/Tests/comment/test_comments.py
@@ -2,6 +2,7 @@ import pytest
 from rest_api_test_framework.src.utilities.logging_utility import logger
 from rest_api_test_framework.src.utilities.request_utility import RequestsApiCall
 from rest_api_test_framework.src.utilities.comments_utility import Comments
+from rest_api_test_framework.src.helpers.random_sentence_generator import generate_random_sentence
 
 pytestmark = [pytest.mark.comments]
 api_request_call = RequestsApiCall()
@@ -31,7 +32,11 @@ class TestCommentsEndpoint:
         :return:
         """
         logger.info("Running tcid03 'test_cud_comment'")
-        # create comment
+        # step 1: create comment w/ a random comment
+        random_string = generate_random_sentence()
+        logger.debug(f"Creating new comment with string: {random_string}")
+        response = comments.create_comment(access_token=get_auth_token, string=random_string)
+        assert response.ok, f"Error! Unexpected status code returned. {response.status_code}"
         # update comment
         # post comment
 
--- rest_api_test_framework/src/utilities/comments_utility.py
+++ rest_api_test_framework/src/utilities/comments_utility.py
@@ -1,21 +1,25 @@
 from rest_api_test_framework.src.utilities.request_utility import RequestsApiCall
 from rest_api_test_framework.src.utilities.build_request_headers_utility import build_request_headers
-from rest_api_test_framework.src.utilities.url_encoding_utility import url_encode_string
+from rest_api_test_framework.src.helpers.url_encoding import url_encode_string
 
 
 class Comments:
 
     def __init__(self):
         self.endpoint = "comments"
+        self.text_header = "/?text="
         self.api_request = RequestsApiCall()
 
     def get_all_comments(self, access_token):
         request_headers = build_request_headers(access_token)
         response = self.api_request.get(endpoint=self.endpoint, api_headers=request_headers)
+        self.api_request.expected_status_code(status_code=response.status_code)
         return response
 
     def create_comment(self, access_token, string):
         request_headers = build_request_headers(access_token)
         encoded_string = url_encode_string(string)
-        response = self.api_request.post(endpoint=f"{self.endpoint}/?{encoded_string}", api_headers=request_headers)
+        response = self.api_request.post(endpoint=f"{self.endpoint}{self.text_header}{encoded_string}",
+                                         api_headers=request_headers)
+        self.api_request.expected_status_code(status_code=response.status_code, expected_status_code=201)
         return response
