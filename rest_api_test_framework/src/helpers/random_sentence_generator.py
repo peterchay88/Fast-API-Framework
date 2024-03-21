92c41ee0f0e482ab3fe3103c0edd5432f3d8025b
Peter Chay <peterchay88@gmail.com>
2024-03-20 20:26:13 -0600

    Added random_sentence_generator.py

---
+++ rest_api_test_framework/src/helpers/random_sentence_generator.py
@@ -0,0 +1,8 @@
+from wonderwords import RandomSentence
+
+random_sentence = RandomSentence()
+
+
+def generate_random_sentence():
+    sentence = random_sentence.bare_bone_sentence()
+    return sentence
