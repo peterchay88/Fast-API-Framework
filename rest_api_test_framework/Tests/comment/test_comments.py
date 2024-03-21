8acde843681fa30d1d3965aec50313decd9fe5dc
Peter Chay <peterchay88@gmail.com>
2024-03-21 14:04:51 -0600

    starting on delete comment on test case 3

--- rest_api_test_framework/Tests/comment/test_comments.py
+++ rest_api_test_framework/Tests/comment/test_comments.py
@@ -34,6 +34,7 @@ class TestCommentsEndpoint:
         logger.info("Running tcid03 'test_cud_comment'")
 
         # step 1: create comment w/ a random comment
+        logger.info("Running tcid03: Creating new comment.")
         random_string = generate_random_sentence()
         logger.debug(f"Creating new comment with string: {random_string}")
         response = comments.create_comment(access_token=get_auth_token, string=random_string)
@@ -44,6 +45,7 @@ class TestCommentsEndpoint:
              f"Actual {response.json()['comment_text']}")
 
         # step 2: update comment via comment id. We also need to pass in number of likes to update to
+        logger.info("Running tcid03: Updating comment.")
         comment_id = response.json()["id"]
         logger.debug(f"Now updating the comment with ID : {comment_id}")
         likes = generate_random_number()
@@ -51,8 +53,13 @@ class TestCommentsEndpoint:
         payload = {"comment_text": new_random_string, "likes": likes}
         logger.debug(f"updating the comment with ID : {comment_id} with the following payload: {payload}")
         response = comments.update_comment(access_token=get_auth_token, comment_id=comment_id, comment_payload=payload)
-        import pdb;pdb.set_trace()
-        # step 3: delete comment
+        assert response.json()['comment_text'] == new_random_string, \
+            (f"Error! Unexpected string for comment. Expected {new_random_string}. "
+             f"Actual {response.json()['comment_text']}")
+        assert response.json()['likes'] == likes, \
+            f"Error! Unexpected value for likes. Expected {likes}. Actual {response.json()['likes']}."
 
-        # trying to figure out why tcid03 won't run. I suspect the issue is somewhere in the headers
+        # step 3: delete comment
+        logger.info("Running tcid03: Deleting comment.")
+
 
