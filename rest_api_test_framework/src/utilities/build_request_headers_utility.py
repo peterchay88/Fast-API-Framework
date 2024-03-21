f3af8d74b16d228787e6ebfa61a3c75400c2af68
Peter Chay <peterchay88@gmail.com>
2024-03-20 21:24:35 -0600

    added kwargs to build_request_headers_utility.py

--- rest_api_test_framework/src/utilities/build_request_headers_utility.py
+++ rest_api_test_framework/src/utilities/build_request_headers_utility.py
@@ -3,6 +3,6 @@ def build_request_headers(access_token, content_type="application/json", **kwarg
         "Authorization": f"Bearer {access_token}",
         "Accept": content_type
     }
-    headers.update(kwargs) # Update headers with additional keyword arguments
+    headers.update(kwargs)  # Update headers with additional keyword arguments
     return headers
 
