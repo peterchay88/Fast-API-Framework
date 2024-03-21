987f432865db4f8794dbfefc67253a78314b9724
Peter Chay <peterchay88@gmail.com>
2024-03-20 20:26:53 -0600

    updated create comment method

--- rest_api_test_framework/src/utilities/comments_utility.py
+++ rest_api_test_framework/src/utilities/comments_utility.py
@@ -1,5 +1,6 @@
 from rest_api_test_framework.src.utilities.request_utility import RequestsApiCall
 from rest_api_test_framework.src.utilities.build_request_headers_utility import build_request_headers
+from rest_api_test_framework.src.utilities.url_encoding_utility import url_encode_string
 
 
 class Comments:
@@ -13,7 +14,8 @@ class Comments:
         response = self.api_request.get(endpoint=self.endpoint, api_headers=request_headers)
         return response
 
-    def create_comment(self, access_token):
+    def create_comment(self, access_token, string):
         request_headers = build_request_headers(access_token)
-        response = self.api_request.post(endpoint=self.endpoint, api_headers=request_headers)
+        encoded_string = url_encode_string(string)
+        response = self.api_request.post(endpoint=f"{self.endpoint}/?{encoded_string}", api_headers=request_headers)
         return response
