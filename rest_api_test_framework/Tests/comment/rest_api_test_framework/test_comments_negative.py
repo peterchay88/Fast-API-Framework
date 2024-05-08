c888f3434a75c7a070722c33965090920a731bac
Peter Chay <peterchay88@gmail.com>
2024-05-08 01:04:35 -0600

    finished negative test case for updating comments passed the character limit.

--- rest_api_test_framework/Tests/comment/rest_api_test_framework/test_comments_negative.py
+++ rest_api_test_framework/Tests/comment/rest_api_test_framework/test_comments_negative.py
@@ -26,3 +26,22 @@ class TestCommentsEndpointNegative:
                                            expected_status_code=500)
         assert response.status_code == 500, \
             f"Unexpected Status code. Expected 500. Actual {response.status_code}"
+
+    @pytest.mark.tcid08
+    def test_update_comment_over_max_char_limit(self, get_auth_token):
+        """
+        This test case is to confirm that you cannot update a comment to something that is more than 140 characters.
+        :param get_auth_token:
+        :return:
+        """
+        logger.info("Running tcid08 'test_update_comments_over_max_char_limit'")
+        # Generate a comment ID to update
+        comment_id = comments.get_random_comment_id(access_token=get_auth_token)
+        # Generate a Random sentence with 141 or More characters. 140 is the limit.
+        random_sentence = generate_random_sentence(char_length=141)
+        logger.debug(f"Attempting to update a comment with ID {comment_id} with {len(random_sentence)} characters")
+        response = comments.update_comment(access_token=get_auth_token, comment_id=comment_id,
+                                           comment_text=random_sentence, expected_status_code=422)
+        assert response.json()['detail'][0]['msg'] == "ensure this value has at most 140 characters", \
+            (f"Unexpected value. Expected ensure this value has at most 140 characters. "
+             f"Actual {response.json()['detail'][0]['msg']}")
